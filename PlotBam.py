665f0fb0602b454be19242310e0f4aeb7f3294b9
Drukewitz, Stephan <stephan.drukewitz@uniklinikum-dresden.de>
2021-03-03 18:32:19 +0100

    added maxcov argument

--- PlotBam.py
+++ PlotBam.py
@@ -12,6 +12,8 @@ requiredNamed.add_argument('-e', '--end',help='end of the region of interest', r
 
 optArguments = parser.add_argument_group('optional arguments')
 optArguments.add_argument('--threads',default=1, help="number of cpu's  to run in paralell, ROI <1000 will always use 1 core")
+optArguments.add_argument('--maxcoverage',default=200, help="max cov to plot",type=int)
+
 optArguments.add_argument('--direction',default=False,action='store_true', help="split reads by forward and reverse")
 optArguments.add_argument('--schematic',default=False,action='store_true', help='plot no nucleotide, recommended for ROI>1000')
 optArguments.add_argument('--style',default='classic', help='different style options for the plot, def:classic',choices=['classic','fancy'])
@@ -25,6 +27,6 @@ if len(sys.argv)==1:
     sys.exit(1)
 
 args=parser.parse_args()
-ploter=PlotMapping(args.bam,args.chromosome,int(args.start),int(args.end),flag=args.flag,schematic=args.schematic,direction=args.direction,coverage=300)
-ploter.PlotNucDir()
+ploter=PlotMapping(args.bam,args.chromosome,int(args.start),int(args.end),flag=args.flag,schematic=args.schematic,direction=args.direction,coverage=args.maxcoverage)
+ploter.Plot()
 #ploter.Plot()

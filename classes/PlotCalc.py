eccdba0934d25817c65f4931b0157b2374acdf52
Drukewitz, Stephan <stephan.drukewitz@uniklinikum-dresden.de>
2021-03-04 16:01:52 +0100

    sizing, title

--- classes/PlotCalc.py
+++ classes/PlotCalc.py
@@ -17,6 +17,8 @@ class CalcPlot():
         self.chunksize=chunksize
 
     def startPlot(self,cols,direction,schematic):
+
+        self.cols=cols
         if not direction and not schematic:
             if cols==1:
                 if self.end-self.start <=100:
@@ -48,44 +50,81 @@ class CalcPlot():
                 y=0.06*self.maxHeight*2
                 fig,ax=plt.subplots(2,cols,figsize=(x*cols,6))
 
-        if not direction and schematic:
-                x=12
-                y=0.12*self.maxHeight
-                fig,ax=plt.subplots(1,cols,figsize=(x,y))
-        if  direction and schematic:
-                x=12
-                y=0.12*self.maxHeight*2
-                fig,ax=plt.subplots(2,cols,figsize=(x,y))
 
+        if not direction and schematic:
+            if self.cols >= 5:
+                y=(0.12*self.maxHeight)/5
+                x=12
+                self.innerLW=0
+                self.LW=0.2
+            else:
+                self.innerLW=0.4/self.cols
+                self.LW=3/self.cols
+                x=12
+                y=(0.12*self.maxHeight)/self.cols
+            fig,ax=plt.subplots(1,self.cols,figsize=(x,y))
+        if  direction and schematic:
+            if self.cols >= 5:
+                y=((0.12*self.maxHeight)/5)*2
+                x=12
+                self.innerLW=0
+                self.LW=0.2
+            else:
+                self.innerLW=0.4/self.cols
+                self.LW=3/self.cols
+                x=12
+                y=((0.12*self.maxHeight)/self.cols)*2
+            fig,ax=plt.subplots(2,self.cols,figsize=(x,y))
+
+        plt.suptitle('chromosome {} from {} to {}'.format(str(self.chrom),str(self.start),str(self.end)))
         self.ax=ax
         self.fig=fig
-        plt.subplots_adjust(wspace=0,hspace=0.01)
+        plt.subplots_adjust(wspace=0,hspace=0.05)
         plt.style.use('bmh')
 
 
     def plotChunk(self,df,ax,start,end,direction=None):
+        df['y']=df['y']+1
+
         for y,s,e,d in zip(df['y'],df['start'],df['end'],df['direction']):
             e=e
             s=s+1
-            if direction and y>(self.maxHeight/2):
+            if y>(self.maxHeight):
+                ax.plot((s,e),(self.maxHeight+1,self.maxHeight+1),color='red',alpha=0.1)
                 continue
             if d=='r':
-                ax.plot((s,e),(y,y),color='grey')
-                ax.plot((s+1,e-1),(y,y),linewidth=0.1,color='white')
+                ax.plot((s,e),(y,y),color='grey',linewidth=self.LW)
+                ax.plot((s+1,e-1),(y,y),linewidth=self.innerLW,color='white')
 
             else:
-                ax.plot((s,e),(y,y),color='black')
-                ax.plot((s+1,e-1),(y,y),linewidth=0.1,color='white')
+                ax.plot((s,e),(y,y),color='black',linewidth=self.LW)
+                ax.plot((s+1,e-1),(y,y),linewidth=self.innerLW,color='white')
 
 
 
-    def AxSet(self,ax,start,end,chunk=False):
+    def AxSet(self,ax,start,end,chunk=False,direction='all'):
+        ylabel=direction
         if not chunk:
-            ax.set(xlim=(start,end),ylim=(0,self.maxHeight+2))
-        else:
-            ax.set(xlim=(start,start+self.chunksize),ylim=(0,self.maxHeight+2))
+            ax.set(xlim=(start,end),ylim=(0,self.maxHeight+2),ylabel=ylabel)
+            ax.set_xticks([start,end],)
+            ax.set_xticklabels([str(start/1_000_000)+' mb',str(end/1_000_000)+' mb'], rotation=40, ha='right')
 
-        ax.get_yaxis().set_visible(False)
+        else:
+            ax.get_yaxis().set_visible(False)
+            ax.set(xlim=(start,start+self.chunksize),ylim=(0,self.maxHeight+2))
+            if start==self.start:
+                ax.set(ylabel=ylabel)
+                ax.get_yaxis().set_visible(True)
+                ax.set_xticks([start])
+                ax.set_xticklabels([str(start/1_000_000)+' mb'], rotation=40, ha='right',fontsize=6)
+            if end==self.end:
+                ax.set_xticks([start,start+self.chunksize])
+                ax.set_xticklabels(['',str((start+self.chunksize)/1_000_000)+' mb'], rotation=40, ha='right',fontsize=6)
+            if  start!=self.start and end!=self.end:
+                ax.set_xticks([start])
+                ax.set_xticklabels([''], rotation=40, ha='right',fontsize=6)
+
+
         ax.spines['right'].set_visible(False)
         ax.spines['left'].set_visible(False)
 
@@ -104,6 +143,7 @@ class CalcPlot():
 
 
     def PlotNucChunk(self,df,ax,start,end,flag='None'):
+        df['y']=df['y']+1
         colorDict={'A':'red','C':'blue','T':'green','G':'yellow','-':'black','N':'pink'}
         for y,s,e,d,qS,cig,mate in zip(df['y'],df['start'],df['end'],df['direction'],df['qSeq'],df['cigar'],df['mateMap']):
             e=e
@@ -123,7 +163,6 @@ class CalcPlot():
             chunk_cigarstringS=[x for x in chunk_cigarstring if x =='S']
             chunk_cigarstring=[x for x in chunk_cigarstring if x !='S']
             softClipp=len(chunk_cigarstringS)/chunkL
-            print(softClipp)
             alpha=1
             if flag=='None':
                 alpha=1

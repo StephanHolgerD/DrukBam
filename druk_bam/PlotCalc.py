52ceb86ad366ba6e094735059e031a5bd5af05f3
Drukewitz, Stephan <stephan.drukewitz@uniklinikum-dresden.de>
2021-03-19 15:44:44 +0100

    scatter points, vcf, style dict

--- druk_bam/PlotCalc.py
+++ druk_bam/PlotCalc.py
@@ -3,10 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from multiprocessing import Pool
-
+import configparser
+import os
+import sys
 class CalcPlot():
-    def __init__(self,mapping,chrom,start,end,threads=1,coverage=200,flag='None',chunksize=1000, fasta=None):
-        plt.style.use('bmh')
+    def __init__(self,mapping,chrom,start,end,threads=1,coverage=200,flag='None',chunksize=1000, fasta=None,style='classic'):
         self.mapping=mapping
         self.chrom=chrom
         self.start=start
@@ -17,7 +18,30 @@ class CalcPlot():
         self.flag=flag
         self.chunksize=chunksize
         self.fasta = fasta
-        self.colorDict={'A':'red','C':'blue','T':'green','G':'yellow','-':'white','N':'pink','a':'red','c':'blue','t':'green','g':'yellow','-':'white','n':'pink'}
+        config = configparser.ConfigParser()
+        if style!=None:
+            if os.path.isfile(style):
+                config.read(style)
+            else:
+                print('{} is not a existing file'.format(style))
+                config.read(os.path.join(os.path.dirname(__file__),'classic.ini'))
+
+        else:
+            config.read(os.path.join(os.path.dirname(__file__),'classic.ini'))
+
+        colorDict={}
+        for a in config['nucleotide color']:
+            colorDict[a.upper()]=config['nucleotide color'][a]
+            colorDict[a.lower()]=config['nucleotide color'][a]
+        for a in config['special chars']:
+            colorDict[a]=config['special chars'][a]
+        for a in config['MatplotStyle']:
+            colorDict[a]=config['MatplotStyle'][a]
+        self.colorDict=colorDict
+        plt.style.use(self.colorDict['pltstyle'])
+
+
+        #self.colorDict={'A':'red','C':'blue','T':'green','G':'yellow','-':'white','N':'pink','a':'red','c':'blue','t':'green','g':'yellow','-':'white','n':'pink','|':'black'}
     def startPlot(self,cols,direction,schematic):
 
         self.cols=cols
@@ -28,9 +52,12 @@ class CalcPlot():
                     y=0.12*self.maxHeight
                     fig,ax=plt.subplots(1,cols,figsize=(x,y))
                     self.Fontsize=6
+                    self.markersize=0.5
                 else:
                     x=0.035*(self.end-self.start)
                     y=0.06*self.maxHeight
+                    self.markersize=0.25
+
                     fig,ax=plt.subplots(1,cols,figsize=(x,y))
             if cols >1:
                 x=0.035*5000
@@ -41,11 +68,14 @@ class CalcPlot():
                 if self.end-self.start <=100:
                     x=0.07*(self.end-self.start)
                     y=0.12*self.maxHeight*2
+                    self.markersize=0.5
+
                     fig,ax=plt.subplots(2,cols,figsize=(x,y))
                     self.Fontsize=6
                 else:
                     x=0.035*(self.end-self.start)
                     y=0.06*self.maxHeight*2
+                    self.markersize=0.25
                     fig,ax=plt.subplots(2,cols,figsize=(x,y))
             if cols >1:
                 x=0.035*5000
@@ -81,7 +111,7 @@ class CalcPlot():
         plt.suptitle('chromosome {} from {} to {}'.format(str(self.chrom),str(self.start),str(self.end)))
         self.ax=ax
         self.fig=fig
-        plt.subplots_adjust(wspace=0,hspace=0.05)
+        plt.subplots_adjust(wspace=0,hspace=0.05, )
         return self.fig,self.ax
 
 
@@ -104,12 +134,16 @@ class CalcPlot():
 
 
 
-    def AxSet(self,ax,start,end,chunk=False,direction='all'):
+    def AxSet(self,ax,start,end,chunk=False,direction='all',vcf=False):
         ylabel=direction
         if not chunk:
             ax.set(xlim=(start,end),ylim=(0,self.maxHeight+2),ylabel=ylabel)
-            ax.set_xticks([start,end],)
-            ax.set_xticklabels([str(start/1_000_000)+' mb',str(end/1_000_000)+' mb'], rotation=40, ha='right')
+            if vcf:
+                ax.set_xticks([start,start+(end-start)/2,end],)
+                ax.set_xticklabels([str(start/1_000_000)+' mb','vcf position',str(end/1_000_000)+' mb'], rotation=40, ha='right')
+            else:
+                ax.set_xticklabels([str(start/1_000_000)+' mb',str(end/1_000_000)+' mb'], rotation=40, ha='right')
+                ax.set_xticks([start,end],)
 
         else:
             ax.get_yaxis().set_visible(False)
@@ -152,7 +186,7 @@ class CalcPlot():
             for _,n in enumerate(fa.fetch(self.chrom,self.start-1,self.end)):
                 ax.text(self.start+_,0,n,fontsize=self.Fontsize,
                         color=self.colorDict[n],alpha=1,family='monospace',ha='center',va='center',
-                        bbox=dict(alpha=1,boxstyle='square,pad=0', fc='black', ec='none'))
+                        bbox=dict(alpha=1,boxstyle='square,pad=0', fc=self.colorDict['fasta background'], ec='none'))
 
     def listConsec(self,l):
         retL=[]
@@ -178,7 +212,7 @@ class CalcPlot():
                     plotC.add(_)
                 if plotC==set(list(range(start,end+1))):
                     return
-                ax.plot((s,e),(self.maxHeight+1,self.maxHeight+1),color='red',alpha=0.1)
+                ax.plot((s,e),(self.maxHeight+1,self.maxHeight+1),color=self.colorDict['max coverage'],alpha=0.1)
                 continue
             chunk_cigarstring=self.CigChunker(cig)
             temp_cig=chunk_cigarstring
@@ -186,9 +220,14 @@ class CalcPlot():
             chunkL=len(chunk_cigarstring)
             chunk_cigarstringS=[x for x in chunk_cigarstring if x =='S' or x =='H']
             chunk_cigarstring=[x for x in chunk_cigarstring if x !='S' and x !='H']
+
             ipos=[x for x,y in enumerate(chunk_cigarstring) if y=='I']
             ipos=self.listConsec(ipos)
             chunk_cigarstring=[x for x in chunk_cigarstring if x !='I']
+            iposCounter=0
+            for i in ipos:
+                query_alignment_sequence="".join([x for _,x in enumerate(query_alignment_sequence) if _+iposCounter not in i])
+                iposCounter=iposCounter+len(i)
 
             for p,_ in enumerate(chunk_cigarstring):
                 if _=='D':
@@ -211,6 +250,9 @@ class CalcPlot():
                 if softClipp>=0.1 or mate:
                     alpha=0.3
             fastapos=0
+            drawChunk=[]
+            if self.fasta != 'None':
+                ax.scatter([x+s for x in range(0,len(chunk_cigarstring))],len(chunk_cigarstring)*[y], marker='o',s=self.markersize,color=self.colorDict['dot'])
             for p,alignPos in enumerate(chunk_cigarstring):
 
                 x=s+p
@@ -222,41 +264,55 @@ class CalcPlot():
                 if alignPos=='S':
                     continue
                 if alignPos=='M':
-                    try:
-                        if self.fasta != 'None' and fastaChunk[fastapos]==query_alignment_sequence[p]:
-                            ax.text(x,y,'.',fontsize=self.Fontsize,
-                                color='black',alpha=alpha,family='monospace',ha='center',va='center',
-                                bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc='none', ec='none'))
-                            fastapos=fastapos+1
-                            continue
-                    except IndexError:
-                        print(cig)
-                        print(chunk_cigarstring)
-                    else:
-                        ax.text(x,y,query_alignment_sequence[p],fontsize=self.Fontsize,
-                            color='black',alpha=alpha,family='monospace',ha='center',va='center',
-                            bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc=self.colorDict[query_alignment_sequence[p]], ec='none'))
+                    if self.fasta != 'None' and fastaChunk[fastapos]==query_alignment_sequence[p]:
                         fastapos=fastapos+1
                         continue
+                        #ax.plot(x,y,color='black', markersize=0.5,marker='o')
+                        #fastapos=fastapos+1
+                        #continue
+                    else:
+                        if self.fasta != 'None':
+                            ax.text(x,y,query_alignment_sequence[p],fontsize=self.Fontsize,
+                                color=self.colorDict['nuc missmatch font'],alpha=alpha,family='monospace',ha='center',va='center',
+                                bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc=self.colorDict[query_alignment_sequence[p]], ec='none'))
+                            fastapos=fastapos+1
+                            continue
+                        else:
+                            ax.text(x,y,query_alignment_sequence[p],fontsize=self.Fontsize,
+                                color=self.colorDict[query_alignment_sequence[p]],alpha=alpha,family='monospace',ha='center',va='center')
+            #    if alignPos=='M':
+            #        if self.fasta != 'None' and fastaChunk[fastapos]==query_alignment_sequence[p]:
+            #            if fastapos<len(fastaChunk)&p<len(query_alignment_sequenceand) and [fastapos+1]==query_alignment_sequence[p+1]:
+            #                drawChunk.append('.')
+            #                fastapos=fastapos+1
+            #                continue
+            #            else:
+            #                x=x-len(drawChunk)
+            #                drawChunk.append('.')
+            #                ax.text(x,y,''.join(drawChunk),fontsize=self.Fontsize,
+            #                    color='black',alpha=alpha,family='monospace',ha='center',va='center')
+            #                fastapos=fastapos+1
+            #                drawChunk=[]
+            #                continue
+            #        else:
+            #            ax.text(x,y,query_alignment_sequence[p],fontsize=self.Fontsize,
+            #                color=self.colorDict[query_alignment_sequence[p]],alpha=alpha,family='monospace',ha='center',va='center')
+                        #ax.text(x,y,query_alignment_sequence[p],fontsize=self.Fontsize,
+                        #    color='black',alpha=alpha,family='monospace',ha='center',va='center',
+                        #    bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc=self.colorDict[query_alignment_sequence[p]], ec='none'))
+            #            fastapos=fastapos+1
+            #            continue
 
                 if alignPos=='D':
                     ax.text(x,y,query_alignment_sequence[p], fontsize=self.Fontsize,ha='center',va='center',family='monospace',
-                            color='black',bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc=self.colorDict[query_alignment_sequence[p]], ec='darkgrey',linewidth=0.00001))
+                            color=self.colorDict['gap font'],bbox=dict(alpha=alpha,boxstyle='square,pad=0', fc=self.colorDict['gap background'], ec='darkgrey',linewidth=0.00001))
                     fastapos=fastapos+1
                     continue
 
             if ipos!=[]:
-                print('--------')
-                print(self.start)
-                print(temp_cig)
-                print(fastaChunk)
-                print(chunk_cigarstring)
-                print(ipos)
-                print(y)
                 for i in ipos:
                     minimum=min(i)
                     x=s+minimum
                     for ii in i:
-                        ax.plot((x,x),(y-0.5,y+0.5),linewidth=0.5)
-                        print(x)
+                        ax.plot((x,x),(y-0.5,y+0.5),linewidth=0.5, color=self.colorDict['insertion'])
                         x=x+0.05

6aeafe5e9b5d611749344e213c4b0e52b382b80c
StephanHolgerD <stephdruk@googlemail.com>
2021-09-25 12:45:07 +0200

    test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+python -m DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction --chunksize 1750
+python -m DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --schematic --direction
+python -m DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --schematic
+
+python -m DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+python -m DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+python -m DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+python -m DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+python -m DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+python -m DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+python -m DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction --chunksize 1750

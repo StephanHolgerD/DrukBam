5b2716afadcd4ef240723e2748558bded0c061f6
StephanHolgerD <stephdruk@googlemail.com>
2021-09-26 12:59:31 +0200

    test bash clipped

---
+++ test_clipped.sh
@@ -0,0 +1,16 @@
+python -m DrukBam region --clipped .15 -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region --clipped .15 -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region --clipped .15 -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region --clipped .15 -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region --clipped .15 -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --direction
+python -m DrukBam region --clipped .15 -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta
+python -m DrukBam region --clipped .15 -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --direction --chunksize 1750
+python -m DrukBam region --clipped .15 -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --schematic --direction
+python -m DrukBam region --clipped .15 -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60  --fasta test_data/chr19_first500k.fasta --schematic
+python -m DrukBam region --clipped .15 -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 
+python -m DrukBam region --clipped .15 -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60   --direction
+python -m DrukBam region --clipped .15 -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60   --direction
+python -m DrukBam region --clipped .15 -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 
+python -m DrukBam region --clipped .15 -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60   --direction
+python -m DrukBam region --clipped .15 -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 
+python -m DrukBam region --clipped .15 -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60   --direction --chunksize 1600

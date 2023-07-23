ac58a6d8a00c27f26bfb332e551539a7b4e1fbbe
StephanHolgerD <stephdruk@googlemail.com>
2023-07-23 12:20:39 +0200

    test for global installation

---
+++ test_global.sh
@@ -0,0 +1,17 @@
+DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction
+DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta
+DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --direction --chunksize 1750
+DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --schematic --direction
+DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff --fasta test_data/chr19_first500k.fasta --schematic
+
+DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+DrukBam region  -s 281367 -e 281468   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+DrukBam region  -s 281367 -e 281466   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction
+DrukBam region  -s 281060 -e 281666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff
+DrukBam region  -s 281060 -e 282666   -c 19 -b test_data/test_small.bam  --outfmt png  -i example_out_small --maxcoverage 60 --outlineoff  --direction --chunksize 1750
